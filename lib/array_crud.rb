ecdce236b342f267688d9731bd0c029e2547a259
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-05 04:16:23 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["mastodon","slipknot","KoRn","opeth"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
