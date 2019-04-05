9eb460e9215b8f86a642e0e6c281ebdcb2609c79
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-05 04:24:54 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  []
+  metal = []
 end
 
 def create_an_array
-  ["mastodon","slipknot","KoRn","opeth"]
+  metal = ["mastodon","slipknot","KoRn","opeth"]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -15,11 +15,11 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
